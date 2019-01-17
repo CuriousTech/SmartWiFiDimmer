55b5105bd1068ad6fa0f835010ede85e868b9515
Greg Cunningham <greg@curioustech.net>
2019-01-16 21:00:43 -0500

    Cap touch dimmer

---
+++ Arduino/control.h
@@ -0,0 +1,31 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+
+#include <Arduino.h>
+
+#define WIFI_LED  14  // top green LED (on high)
+
+class swControl
+{
+public:
+  swControl();
+  void listen(void);
+  void init(void);
+  void setSwitch(bool bOn);
+  void setLevel(void);
+  void setLevel(uint8_t level);
+  void setLED(bool bOn);
+
+  bool    m_bLightOn;      // state
+  uint8_t m_nLightLevel = 50; // current level
+  const uint8_t nLevelMin = 0;
+  const uint8_t nLevelMax = 255;
+private:
+  bool writeSerial(uint8_t cmd, uint8_t *p, uint8_t len);
+  void checkStatus(void);
+
+  uint8_t m_nNewLightLevel = 50; // set in a callback
+  uint8_t m_cs = 1;
+};
+
+#endif // CONTROL_H
