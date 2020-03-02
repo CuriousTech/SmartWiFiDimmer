57e3b38102c0df83b906e710d6760d643b6cbc51
Greg Cunningham <greg@curioustech.net>
2020-03-01 19:06:56 -0500

    Create control.h

---
+++ Arduino/TuyaModule/control.h
@@ -0,0 +1,28 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+
+#include <Arduino.h>
+
+class swControl
+{
+public:
+  swControl();
+  void listen(void);
+  void init(void);
+  void setSwitch(bool bOn);
+  void setLevel(void);
+  void setLevel(uint8_t level);
+  void setLED(uint8_t no, bool bOn);
+
+  bool    m_bLightOn;      // state
+  bool    m_bLED[2];
+  uint8_t m_nLightLevel = 50; // current level
+  const uint8_t nLevelMin = 0;
+  const uint8_t nLevelMax = 255;
+private:
+  bool writeSerial(uint8_t level);
+
+  uint8_t m_nNewLightLevel = 50; // set in a callback
+};
+
+#endif // CONTROL_H
