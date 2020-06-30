398968c954735fb1810d3ac6647930918a24ebdc
Greg Cunningham <greg@curioustech.net>
2020-06-30 15:54:27 -0400

    Update control.h

--- Arduino/dimModule/control.h
+++ Arduino/dimModule/control.h
@@ -15,17 +15,19 @@ public:
   void setLevel(void);
   void setLevel(uint8_t level);
   void setLED(uint8_t no, bool bOn);
+  uint8_t getPower(void);
 
   bool    m_bLightOn;      // state
   bool    m_bLED[2];
-  uint8_t m_nLightLevel = 50; // current level
+  uint8_t m_nLightLevel = 100; // current level
   const uint8_t nLevelMin = 0;
   const uint8_t nLevelMax = 255;
+  const uint8_t nWattMin = 60; // 60% of full watts at lowest
 private:
   bool writeSerial(uint8_t cmd, uint8_t *p, uint8_t len);
   void checkStatus(void);
 
-  uint8_t m_nNewLightLevel = 50; // set in a callback
+  uint8_t m_nNewLightLevel = 100; // set in a callback
   uint8_t m_cs = 1;
 };
 
