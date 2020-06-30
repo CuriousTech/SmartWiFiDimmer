d7a531aa02b0ea0afd2cefbdf9424c88a71bf5d4
Greg Cunningham <greg@curioustech.net>
2020-06-30 15:53:00 -0400

    Update control.h

--- Arduino/TuyaModule/control.h
+++ Arduino/TuyaModule/control.h
@@ -12,16 +12,18 @@ public:
   void setSwitch(bool bOn);
   void setLevel(uint8_t level);
   void setLED(uint8_t no, bool bOn);
+  uint8_t getPower(void);
 
   bool    m_bLightOn;      // state
   bool    m_bLED[2];
-  uint8_t m_nLightLevel = 50; // current level
-  const uint8_t nLevelMin = 16; // flickers below about 15
+  uint8_t m_nLightLevel = 100; // current level
+  const uint8_t nLevelMin = 4;
   const uint8_t nLevelMax = 255;
+  const uint8_t nWattMin = 60; // 60% of full watts at lowest
 private:
   bool writeSerial(uint8_t level);
 
-  uint8_t m_nNewLightLevel = 50; // set in a callback
+  uint8_t m_nNewLightLevel = 100; // set in a callback
 };
 
 #endif // CONTROL_H
