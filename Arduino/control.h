8415dbd98fd95d4c87432a251ac8df2ec2861f9c
Greg Cunningham <greg@curioustech.net>
2020-07-14 19:06:24 -0400

    Merging many

--- Arduino/control.h
+++ Arduino/control.h
@@ -3,7 +3,7 @@
 
 #include <Arduino.h>
 
-#define WIFI_LED  14  // top green LED (on high)
+#define USER_RANGE 200
 
 class swControl
 {
@@ -15,20 +15,19 @@ public:
   void setLevel(void);
   void setLevel(uint8_t level);
   void setLED(uint8_t no, bool bOn);
-  uint8_t getPower(void);
-
+  uint8_t getPower(uint8_t nLevel);
   bool    m_bLightOn;      // state
   bool    m_bLED[2];
-  uint8_t m_nLightLevel = 128; // current level
-  const uint8_t nLevelMin = 1;
-  const uint8_t nLevelMax = 255;
-  const uint8_t nWattMin = 60; // 60% of full watts at lowest
+  uint8_t m_nLightLevel = USER_RANGE / 2; // current level
 private:
-  bool writeSerial(uint8_t cmd, uint8_t *p, uint8_t len);
+  bool writeSerial(uint8_t cmd, uint8_t *p = NULL, uint8_t len = 0);
   void checkStatus(void);
 
-  uint8_t m_nNewLightLevel = 128; // set in a callback
+  uint8_t m_nNewLightLevel = USER_RANGE / 2; // set in a callback
   uint8_t m_cs = 1;
+  uint16_t nLevelMin = 25; // hardware range
+  uint16_t nLevelMax = 255;
+  const uint8_t nWattMin = 60; // 60% of full watts at lowest
 };
 
 #endif // CONTROL_H
