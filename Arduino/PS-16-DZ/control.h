ade75332b04134d0bde4a5a56733f9a1e0c5cf63
Greg Cunningham <greg@curioustech.net>
2020-07-25 23:02:32 -0400

    Update control.h

--- Arduino/PS-16-DZ/control.h
+++ Arduino/PS-16-DZ/control.h
@@ -3,7 +3,7 @@
 
 #include <Arduino.h>
 
-#define WIFI_LED  13  // top green LED (on low)
+#define USER_RANGE 200
 
 class swControl
 {
@@ -14,17 +14,24 @@ public:
   void setSwitch(bool bOn);
   void setLevel(void);
   void setLevel(uint8_t level);
+  void test(uint8_t cmd, uint16_t v);
   void setLED(uint8_t no, bool bOn);
-  uint8_t getPower(void);
-
+  uint8_t getPower(uint8_t nLevel);
   bool    m_bLightOn;      // state
   bool    m_bLED[2];
-  uint8_t m_nLightLevel = 50; // current level
-  const uint8_t nLevelMin = 10;
-  const uint8_t nLevelMax = 99;
-  const uint8_t nWattMin = 60; // 60% of full watts at lowest
+  uint8_t m_nLightLevel = USER_RANGE / 2; // current level
+  float   m_fVolts;
+  float   m_fCurrent;
+  float   m_fPower;
 private:
-  uint8_t m_nNewLightLevel = 100; // set in a callback
+  bool writeSerial(uint8_t cmd, uint8_t *p = NULL, uint16_t len = 0);
+  void checkStatus(void);
+
+  uint8_t m_nNewLightLevel = USER_RANGE / 2; // set in a callback
+  uint8_t m_cs = 1;
+  uint16_t nLevelMin = 25; // hardware range
+  uint16_t nLevelMax = 255;
+  const uint8_t nWattMin = 60; // 60% of full watts at lowest
 };
 
 #endif // CONTROL_H
