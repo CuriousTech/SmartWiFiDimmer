84ae9a17e1f17c7388e1c7b5a211fb4492a79b28
Greg Cunningham <greg@curioustech.net>
2020-07-25 23:04:57 -0400

    Update control.h

--- Arduino/TuyaModule/control.h
+++ Arduino/TuyaModule/control.h
@@ -12,13 +12,17 @@ public:
   void setSwitch(bool bOn);
   void setLevel(uint8_t level);
   void setLED(uint8_t no, bool bOn);
-  uint8_t getPower(void);
-
+  uint8_t getPower(uint8_t nLevel);
+  void test(uint8_t cmd, uint16_t v);
   bool    m_bLightOn;      // state
   bool    m_bLED[2];
   uint8_t m_nLightLevel = 100; // current level
+  float   m_fVolts;
+  float   m_fCurrent;
+  float   m_fPower;
   const uint8_t nLevelMin = 4;
   const uint8_t nLevelMax = 255;
+  const uint8_t m_nUserRange = 200;
   const uint8_t nWattMin = 60; // 60% of full watts at lowest
 private:
   bool writeSerial(uint8_t level);
