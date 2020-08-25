a6e98787c53018dcc9e8a550d43d4a9e6f6b2364
Greg Cunningham <greg@curioustech.net>
2020-08-25 18:56:05 -0400

    Update control.h

--- Arduino/control.h
+++ Arduino/control.h
@@ -3,14 +3,12 @@
 
 #include <Arduino.h>
 
-#define USER_RANGE 200
-
 class swControl
 {
 public:
   swControl();
   void listen(void);
-  void init(void);
+  void init(uint8_t nRange);
   void setSwitch(bool bOn);
   void setLevel(void);
   void setLevel(uint8_t level);
@@ -19,7 +17,7 @@ public:
   uint8_t getPower(uint8_t nLevel);
   bool    m_bLightOn;      // state
   bool    m_bLED[2];
-  uint8_t m_nLightLevel = USER_RANGE / 2; // current level
+  uint8_t m_nLightLevel; // current level
   float   m_fVolts;
   float   m_fCurrent;
   float   m_fPower;
@@ -27,7 +25,8 @@ private:
   bool writeSerial(uint8_t cmd, uint8_t *p = NULL, uint16_t len = 0);
   void checkStatus(void);
 
-  uint8_t m_nNewLightLevel = USER_RANGE / 2; // set in a callback
+  uint8_t m_nUserRange;
+  uint8_t m_nNewLightLevel; // set in a callback
   uint8_t m_cs = 1;
   uint16_t nLevelMin = 25; // hardware range
   uint16_t nLevelMax = 255;
