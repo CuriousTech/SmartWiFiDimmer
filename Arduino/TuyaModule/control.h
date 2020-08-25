c89f0242afee3d30d897f70d1c6429bb9b615f0c
Greg Cunningham <greg@curioustech.net>
2020-08-25 18:56:51 -0400

    Update control.h

--- Arduino/TuyaModule/control.h
+++ Arduino/TuyaModule/control.h
@@ -8,7 +8,7 @@ class swControl
 public:
   swControl();
   void listen(void);
-  void init(void);
+  void init(uint8_t nUserRange);
   void setSwitch(bool bOn);
   void setLevel(uint8_t level);
   void setLED(uint8_t no, bool bOn);
@@ -22,12 +22,12 @@ public:
   float   m_fPower;
   const uint8_t nLevelMin = 4;
   const uint8_t nLevelMax = 255;
-  const uint8_t m_nUserRange = 200;
   const uint8_t nWattMin = 60; // 60% of full watts at lowest
 private:
   bool writeSerial(uint8_t level);
 
   uint8_t m_nNewLightLevel = 100; // set in a callback
+  uint8_t m_nUserRange = 200;
 };
 
 #endif // CONTROL_H
