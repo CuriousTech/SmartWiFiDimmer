9b2c4d095ec6eb45b2ce27b9f1098fdcbfd5fe87
Greg Cunningham <greg@curioustech.net>
2021-09-21 01:53:27 -0400

    Update control.h

--- Arduino/control.h
+++ Arduino/control.h
@@ -8,19 +8,21 @@ class swControl
 public:
   swControl();
   void listen(void);
-  void init(uint8_t nRange);
+  void init(uint8_t nUserRange);
+  char *getDevice(void);
   void setSwitch(bool bOn);
   void setLevel(void);
   void setLevel(uint8_t level);
-  void test(uint8_t cmd, uint16_t v);
   void setLED(uint8_t no, bool bOn);
   uint8_t getPower(uint8_t nLevel);
   bool    m_bLightOn;      // state
   bool    m_bLED[2];
+  uint8_t m_nBlink;
   uint8_t m_nLightLevel; // current level
   float   m_fVolts;
   float   m_fCurrent;
   float   m_fPower;
+  bool    m_bOption;
 private:
   bool writeSerial(uint8_t cmd, uint8_t *p = NULL, uint16_t len = 0);
   void checkStatus(void);
