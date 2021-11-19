4687801ae461e4ef8b1992871204ad51aca63610
Greg Cunningham <greg@curioustech.net>
2021-11-19 00:23:01 -0500

    Create Module.cpp

---
+++ Arduino/Module.cpp
@@ -0,0 +1,107 @@
+// Class for controlling wired Tuya dimmer module QS-WIFI-D02-TRIAC
+
+#include "Module.h"
+#include <TimeLib.h>
+#include "eeMem.h"
+
+#define SWITCH_IN 13
+
+Module::Module()
+{
+  pinMode(SWITCH_IN, INPUT);
+}
+
+void Module::init(uint8_t nUserRange)
+{
+  m_nUserRange = nUserRange;
+  Serial.begin(9600);
+}
+
+char *Module::getDevice()
+{
+  return "INLINE_CUBE";
+}
+
+uint8_t Module::getPower(uint8_t nLevel)
+{
+  return map(nLevel, 0, m_nUserRange, nWattMin, 100);  // 1% = about 60% power
+}
+
+void Module::listen()
+{
+  // handle 60Hz AC switch input (low pulse while pressed)
+  static int cnt;
+  static uint32_t tm;
+  static int8_t nDirection;
+  if(digitalRead(SWITCH_IN) == LOW) // catches at around 20-400ms
+  {
+    cnt++;
+    tm = millis();
+    if(cnt > 4) // holding
+    {
+      if(nDirection == 0) // set direction
+      {
+        if(m_bLightOn == false)
+          m_bLightOn = true;
+        if(m_nLightLevel < m_nUserRange)
+            nDirection = 1; // up
+        else
+            nDirection = 2; // down
+      }
+      if(nDirection == 1)
+      {
+        if(m_nLightLevel < m_nUserRange)
+          m_nLightLevel++;
+      }
+      else
+      {
+        if(m_nLightLevel > 1)
+          m_nLightLevel--;
+      }
+    }
+  }
+  else if((millis() - tm) > 500 && cnt) // >500ms = release
+  {
+    if(cnt <= 4) // short tap (~100ms)
+      m_bLightOn = !m_bLightOn;
+    cnt = 0;
+//    nDirection = 0; // reset will be up next time
+  }
+
+  uint8_t s = second();
+  static uint8_t ls;
+  if(s != ls) // Set new light value every second
+  {
+    ls = s;
+    uint8_t level = map(m_nLightLevel, 1, m_nUserRange, nLevelMin, nLevelMax);
+    writeSerial( m_bLightOn ? level : 0);
+  }
+}
+
+void Module::setSwitch(bool bOn)
+{
+  m_bLightOn = bOn;
+}
+
+bool Module::writeSerial(uint8_t level)
+{
+  uint8_t buf[6];
+
+  buf[0] = 0xFF;
+  buf[1] = 0x55;
+  buf[2] = level;
+  buf[3] = 0x05; // 05DC = 1500?
+  buf[4] = 0xDC;
+  buf[5] = 0x0A;
+
+  return Serial.write(buf, 6);
+}
+
+void Module::setLevel(uint8_t n)
+{
+  m_nNewLightLevel = constrain(n, 1, m_nUserRange);
+}
+
+void Module::setLED(uint8_t no, bool bOn)
+{
+}
