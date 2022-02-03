52eb5209fe8aeb0057dc10fdab6265457cd1dad6
Greg Cunningham <greg@curioustech.net>
2022-02-03 15:08:09 -0500

    Added simple RLE

--- Arduino/jsonstring.h
+++ Arduino/jsonstring.h
@@ -154,6 +154,64 @@ public:
     m_cnt++;
   }
 
+  int RLE(char *key, float fVal[], int n, int nTotal)
+  {
+    if(m_cnt) s += ",";
+    s += "\"";
+    s += key;
+    s += "\":[";
+
+    for(int cnt = 0; n < nTotal && s.length() < 750; cnt++)
+    {
+      if(cnt) s += ",";
+
+      s += "[";
+
+      float fV = fVal[n++];
+      int i;
+
+      if(fV != fVal[n]) // do bulk
+      {
+        int i;
+        int len = 0;
+        float fV2 = fVal[n++];
+        int n2 = n;
+        for(i = 0; fV2 != fVal[n2] && n2 < nTotal && len + s.length() < 750; i++, n2++) // lookahead, just look for 1 repeat
+        {
+          fV2 = fVal[n2];
+          String sl = String(fV2);
+          len += sl.length()+1;
+        }
+        s += i + 1;
+        s += ",";
+        s += fV;
+        for(int j = 0; j < i; j++, n++)
+        {
+          s += ",";
+          fV = fVal[n];
+          if(fV == (int)fV)
+            s += (int)fV;
+          else
+            s += fV;
+        }
+      }
+      else // do repeat
+      {
+        for(i = 1; fV == fVal[n] && n < nTotal; i++, n++);
+        s += -i;
+        s += ",";
+        if(fV == (int)fV) // remove a few chars if possible
+          s += (int)fV;
+        else
+          s += fV;
+      }
+      s += "]";
+    }
+    s += "]";
+    m_cnt++;
+    return n;
+  }
+
   void Array(char *key, Sched sch[], int n)
   {
     if(m_cnt) s += ",";
@@ -192,7 +250,7 @@ public:
       s += "\",";  s += dev[i].mode;
       s += ","; s += dev[i].flags;
       s += ","; s += dev[i].delay;
-      s += ","; s += devst[i].bOn;
+      s += ","; s += devst[i].bPwr;
       s += ","; s += devst[i].nLevel;
       s += "]";
     }
