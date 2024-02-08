bfb47b83e276607e135e72a532a386cc43d8df4f
Greg Cunningham <greg@curioustech.net>
2024-02-08 00:04:51 -0500

    Update eeMem.h

--- Arduino/eeMem.h
+++ Arduino/eeMem.h
@@ -47,17 +47,18 @@ struct Device
   uint16_t delay;
   uint8_t mode; // DEV_MODE
   uint8_t flags; // DEV_FLAGS
-}; // 40
+  uint8_t chns; // channels
+}; // 42
 
 struct DevState
 {
-  bool bPwr;
   uint8_t nLevel;
+  uint8_t nLevelS;
   uint32_t tm;
   bool bChanged;
   uint8_t cmd;
-  bool bPwrS;
-  uint8_t nLevelS;
+  bool bPwr[4];
+  bool bPwrS[4];
 };
 
 struct Energy
@@ -69,11 +70,13 @@ struct Energy
 struct flags_t
 {
   uint16_t call:1;
-  uint16_t lightOn:1;
-  uint16_t start:2;
+  uint16_t lightOn0:1;
+  uint16_t lightOn1:1;
+  uint16_t start0:3;
+  uint16_t start1:3;
+  uint16_t led0:2;
   uint16_t led1:2;
-  uint16_t led2:2;
-  uint16_t res:8;
+  uint16_t res:3;
 };
 
 #define EESIZE (offsetof(eeMem, end) - offsetof(eeMem, size) )
@@ -90,35 +93,34 @@ public:
   uint16_t sum = 0xAAAA;           // if sum is diiferent from memory struct, write
   char     szSSID[32] = ""; // Enter your SSID here
   char     szSSIDPassword[64] = ""; // and SSID password
-  uint8_t  controlIP[4] = {192,168,31,191};
-  uint16_t controlPort = 80;
-  char     reserved[26];
-  bool     bUseNtp;
-  bool     bNotUsed;
-  int8_t   tz = -5;
-  flags_t  flags1 = {0};
-  char     szControlPassword[32] = "password"; // password for WebSocket and HTTP params
+  char     szName[28] = "BackLight"; // Device/OTA name
   uint8_t  hostIP[4] = {192,168,31,100}; // Control/status hub
   uint16_t hostPort = 80;
-  char     szName[28] = "Garage"; // Device/OTA name
+  uint8_t  controlIP[4] = {192,168,31,191}; // A secondary controller
+  uint16_t controlPort = 80;
+  int8_t   tz = -5;
+  bool     bUseNtp;
+  uint16_t watts = 7;  // Fixed watts of devices without metering
+  uint16_t ppkw = 150;  // 15.0 cents/KWH
+  uint8_t  motionPin[2] = {0, 0};   // Basement=14, Back switch=14
+  uint8_t  nLightLevel = 100; // 50%
+  flags_t  flags1 = {0};
+  char     szControlPassword[32] = "password"; // password for WebSocket and HTTP params
   uint32_t autoTimer;
   uint16_t nMotionSecs;
-  uint16_t watts = 23;  // Fixed watts of device
-  uint16_t ppkw = 143;  // 14.3 cents/KWH
-  uint8_t  nLightLevel = 100; // 50%
   float    fTotalWatts;
   uint32_t nTotalSeconds;
   uint32_t nTotalStart;
-  uint8_t  motionPin[2];   // motionPin  Basement=14, Back switch=14
   Sched    schedule[MAX_SCHED] =  // 30*28
   {
     {  6*60,  10*60, 254, 100, "Morning"},  // time, seconds, wday, level, name
-    { 12*60,  60*60, 254, 60, "Lunch"},
+    { 12*60,  60*60, 254, 200, "Lunch"},
     { 14*60, 120*60, 254, 20, "Something"},
   };
-  Device   dev[MAX_DEV];
-  Energy days[31]; // 248 the esp-07 EEPROM seems to be smaller than ESP-12?
-  Energy months[12]; // 96
+  Device  dev[MAX_DEV];
+  Energy  days[31]; // 248 the esp-07 EEPROM seems to be smaller than ESP-12?
+  Energy  months[12]; // 96
+  uint8_t res[32];
   uint8_t end; // marker for EEPROM data size and end
 }; // 2272 + Energy = 2653
 
