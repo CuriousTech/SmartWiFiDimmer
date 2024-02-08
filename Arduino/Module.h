5165b94e8368259fd5d1519abf00b3b12d51bfcf
Greg Cunningham <greg@curioustech.net>
2024-02-08 00:00:09 -0500

    Update Module.h

--- Arduino/Module.h
+++ Arduino/Module.h
@@ -9,12 +9,13 @@ public:
   Module();
   bool listen(void);
   void init(uint8_t nUserRange);
+  void isr(void);
   const char *getDevice(void);
-  void setSwitch(bool bOn);
+  void setSwitch(uint8_t idx, bool bOn);
   void setLevel(uint8_t level);
   void setLED(uint8_t no, bool bOn);
   uint8_t getPower(uint8_t nLevel);
-  bool    m_bPower;      // state
+  bool    m_bPower[2];      // state
   bool    m_bLED[2];
   uint8_t m_nLightLevel = 100; // current level
   float   m_fVolts;
@@ -22,6 +23,7 @@ public:
   float   m_fPower;
   uint8_t m_nBlink;
   bool    m_bOption;
+  int16_t  m_buttonHoldCount;
   const uint8_t nLevelMin = 4;
   const uint8_t nLevelMax = 255;
   const uint8_t nWattMin = 60; // 60% of full watts at lowest
