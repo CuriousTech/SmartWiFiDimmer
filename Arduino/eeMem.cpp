84111d47f4c6094a7a92d46097ef2ed7f389723b
Greg Cunningham <greg@curioustech.net>
2024-10-04 05:29:54 -0400

    ESP32 support

--- Arduino/eeMem.cpp
+++ Arduino/eeMem.cpp
@@ -1,24 +1,36 @@
 #include "eeMem.h"
 #include <EEPROM.h>
+#ifdef ESP32
+#include <SPIFFS.h>
+#endif
 
-eeMem::eeMem()
+extern void WsSend(String s);
+
+void eeMem::init()
 {
-  EEPROM.begin(EESIZE);
-
   uint8_t data[EESIZE];
   uint16_t *pwTemp = (uint16_t *)data;
+
+#ifdef ESP32
+  File F = SPIFFS.open("/eemem", "r");
+  F.read((byte*)data, EESIZE);
+  F.close();
+
+#else
+  EEPROM.begin(EESIZE);
 
   int addr = 0;
   for(int i = 0; i < EESIZE; i++, addr++)
     data[i] = EEPROM.read( addr );
-
+#endif
   if(pwTemp[0] != EESIZE)
     return; // revert to defaults if struct size changes
-
   uint16_t sum = pwTemp[1];
   pwTemp[1] = 0;
   pwTemp[1] = Fletcher16(data, EESIZE );
-  if(pwTemp[1] != sum) return; // revert to defaults if sum fails
+  if(pwTemp[1] != sum)
+    return; // revert to defaults if sum fails
+  
   memcpy(this + offsetof(eeMem, size), data, EESIZE );
 }
 
@@ -31,11 +43,18 @@ void eeMem::update() // write the settings if changed
   if(old_sum == ee.sum)
     return; // Nothing has changed?
 
+#ifdef ESP32
+  File F = SPIFFS.open("/eemem", "w");
+  F.write((byte*) this + offsetof(eeMem, size), EESIZE);
+  F.close();
+
+#else
   uint16_t addr = 0;
   uint8_t *pData = (uint8_t *)this + offsetof(eeMem, size);
   for(int i = 0; i < EESIZE; i++, addr++)
     EEPROM.write(addr, pData[i] );
   EEPROM.commit();
+#endif
 }
 
 uint16_t eeMem::Fletcher16( uint8_t* data, int count)
