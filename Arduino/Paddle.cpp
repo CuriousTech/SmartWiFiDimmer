1db73c4af402fc3b7bd3c4336b43cbec93c47411
Greg Cunningham <greg@curioustech.net>
2024-10-04 05:24:50 -0400

    Update Paddle.cpp

--- Arduino/Paddle.cpp
+++ Arduino/Paddle.cpp
@@ -21,9 +21,9 @@ const char *Paddle::getDevice()
   return "PADDLE";
 }
 
-uint8_t Paddle::getPower(uint8_t nLevel)
+uint8_t Paddle::getPower(void)
 {
-  return map(nLevel, 0, m_nUserRange, nWattMin, 100);  // 1% = about 60% power
+  return map(m_nLightLevel[0], 0, m_nUserRange, nWattMin, 100);  // 1% = about 60% power
 }
 
 bool Paddle::listen()
@@ -51,7 +51,7 @@ bool Paddle::listen()
       if((p = strstr(inBuffer, "bright")) != NULL) // "bright":99
       {
         m_nNewLightLevel = map(atoi(p + 8), nLevelMin, nLevelMax, 0, m_nUserRange); // 10-99
-        m_nLightLevel = m_nNewLightLevel;
+        m_nLightLevel[0] = m_nNewLightLevel;
       }
       String s = "AT+SEND=ok\x1B"; // respond with ok
       Serial.write((uint8_t*)s.c_str(), s.length());
@@ -63,9 +63,9 @@ bool Paddle::listen()
     }
   }
 
-  if(m_nNewLightLevel != m_nLightLevel) // new requested level
+  if(m_nNewLightLevel != m_nLightLevel[0]) // new requested level
   {
-    m_nLightLevel = m_nNewLightLevel;
+    m_nLightLevel[0] = m_nNewLightLevel;
     setLevel();
   }
 
@@ -85,7 +85,7 @@ void Paddle::setSwitch(uint8_t idx, bool bOn)
 {
   String s;
   s = "AT+UPDATE=\"sequence\":\"";
-  s += (now() - ( (ee.tz ) * 3600)*1000) + millis();
+  s += now()*1000 + millis();
   s += "\",\"switch\":\"";
   s += (bOn)? "on" : "off";
   s += "\"\x1B";
@@ -97,16 +97,16 @@ void Paddle::setLevel()
 {
   String s;
   s = "AT+UPDATE=\"sequence\":\"";
-  s += (now() - ( (ee.tz ) * 3600)*1000) + millis();
+  s += now()*1000 + millis();
   s += "\",\"bright\":";
-  s += constrain(m_nLightLevel, nLevelMin, nLevelMax);
+  s += constrain(m_nLightLevel[0], nLevelMin, nLevelMax);
   s += "\x1B";
   Serial.write((uint8_t*)s.c_str(), s.length());
 }
 
-void Paddle::setLevel(uint8_t n)
+void Paddle::setLevel(uint8_t idx, uint8_t level)
 {
-  m_nNewLightLevel = constrain(n, 0, m_nUserRange);
+  m_nNewLightLevel = constrain(level, 0, m_nUserRange);
 }
 
 void Paddle::setLED(uint8_t no, bool bOn)
