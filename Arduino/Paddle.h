c5f6e153d1ca18548ab62ddc766ed32a943f6e5d
Greg Cunningham <greg@curioustech.net>
2024-10-04 05:25:10 -0400

    Update Paddle.h

--- Arduino/Paddle.h
+++ Arduino/Paddle.h
@@ -13,14 +13,13 @@ public:
   void init(uint8_t nUserRange);
   const char *getDevice(void);
   void setSwitch(uint8_t idx, bool bOn);
-  void setLevel(void);
-  void setLevel(uint8_t level);
+  void setLevel(uint8_t idx, uint8_t level);
   void setLED(uint8_t no, bool bOn);
-  uint8_t getPower(uint8_t nLevel);
+  uint8_t getPower(void);
 
   bool    m_bPower[2];      // state
   bool    m_bLED[2];
-  uint8_t m_nLightLevel = 50; // current level
+  uint8_t m_nLightLevel[2] = {50, 0}; // current level
   const uint8_t nLevelMin = 10;
   const uint8_t nLevelMax = 99;
   const uint8_t nWattMin = 60; // 60% of full watts at lowest
@@ -30,6 +29,7 @@ public:
   uint16_t m_nBlink;
   bool  m_bOption;
 private:
+  void setLevel(void);
   uint8_t m_nUserRange;
   uint8_t m_nNewLightLevel = 100; // set in a callback
 };
